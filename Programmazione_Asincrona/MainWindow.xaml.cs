fd25390545bccda9575320dd0d0cf81c0fcb7bcc
Matteo Sartini <MATTEO.SARTINI@local.ittcesena.it>
2026-03-06 09:48:00 +0100

    Manca la stampa delle lettere per comporre la parola

--- Programmazione_Asincrona/MainWindow.xaml.cs
+++ Programmazione_Asincrona/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,37 @@ namespace Programmazione_Asincrona
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        string[] alfabeto = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z", "X", "Y","Z" };
+        int i = 0;
+        string lettera = "";
+        private async void btnEstrai_Click(object sender, RoutedEventArgs e)
+        {
+            parola.Add(lettera);
+            lblParola.Content = parola;
+            await Task.Delay(100);
+        }
+
+        List<string> parola = new List<string>();
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            while (true)
+            {
+                if (i == 26)
+                {
+                    i = -1;
+                }
+                i++;
+                lettera = alfabeto[i];
+                lblLabel.Content = lettera;
+                await Task.Delay(100);
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
